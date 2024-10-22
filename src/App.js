b8e422b3a6fcaa35ba08a0f3cdc4225a89900238
frankspaceyhelder <franksh14@gmail.com>
2024-10-22 14:32:45 +0200

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import {
+  OPEN_WEATHER_API_URL,
+  OPEN_WEATHER_API_KEY,
+} from "./components/search/api";
+import { useState } from "react";
+import Forecast from "./components/forecast/forecast";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    let [lat, lon] = searchData.value.split(" ");
+    lat = lat.trim();
+    lon = lon.trim();
+
+    lat = parseFloat(lat);
+    lon = parseFloat(lon);
+
+    console.log("Latitude:", lat);
+    console.log("Longitude:", lon);
+
+    if (isNaN(lat) || isNaN(lon)) {
+      console.error("Invalid latitude or longitude");
+      return;
+    }
+
+    const currentWeatherFetch = fetch(
+      `${OPEN_WEATHER_API_URL}/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
+    );
+
+    const forecastFetch = fetch(
+      `${OPEN_WEATHER_API_URL}/2.5/forecast?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  };
+
+  console.log(forecast);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
