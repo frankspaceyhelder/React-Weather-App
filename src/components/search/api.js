46f1e6c78b328d068e64cc5175cc2318d2b2eb79
frankspaceyhelder <franksh14@gmail.com>
2024-10-22 14:52:00 +0200

    project done

--- src/components/search/api.js
+++ src/components/search/api.js
@@ -1,13 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "9cfe7ceea6msh1ec7c82c9aff4d1p1a9818jsncde392601f7f",
+    "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
     "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
-
 export const OPEN_WEATHER_API_URL = "https://api.openweathermap.org/data";
-
-export const OPEN_WEATHER_API_KEY = "7c9753b956ec4f5de076535fd564eac1";
+export const OPEN_WEATHER_API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
